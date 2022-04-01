062d5aa76565d4df6db416d89902303ecd36a652
Andrey Shagov <writeletterto@me.com>
2022-04-01 23:05:49 +0300

    Add fonts, scss, 50% of header

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@ import autoprefixer from 'gulp-autoprefixer';
 import uglify from 'gulp-uglify';
 import bs from 'browser-sync';
 import imagemin from 'gulp-imagemin';
-import {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+import { gifsicle, mozjpeg, optipng, svgo } from 'gulp-imagemin';
 import del from 'del';
+import fs from 'fs';
+import fonter from 'gulp-fonter'
+import ttf2woff2 from 'gulp-ttf2woff2';
 
 const scss = gulpSass(dartSass);
 const browserSync = bs.create();
@@ -33,7 +36,7 @@ export const clearDist = () => {
 
 export const styles = () => {
   return gulp.src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
@@ -58,17 +61,97 @@ export const scripts = () => {
 export const images = () => {
   return gulp.src('app/images/**/*.*')
     .pipe(imagemin([
-      gifsicle({interlaced: true}),
-      mozjpeg({quality: 75, progressive: true}),
-      optipng({optimizationLevel: 5}),
+      gifsicle({ interlaced: true }),
+      mozjpeg({ quality: 75, progressive: true }),
+      optipng({ optimizationLevel: 5 }),
       svgo({
         plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
+          { removeViewBox: true },
+          { cleanupIDs: false }
         ]
       })
     ]))
     .pipe(gulp.dest('dist/images'));
+}
+
+// Fonts
+
+export const convertFonts = () => {
+  return gulp.src('app/fonts/*ttf')
+    .pipe(fonter({
+      formats: ['woff']
+    }))
+    .pipe(gulp.dest('app/fonts'))
+    .pipe(gulp.src('app/fonts/*ttf'))
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest('app/fonts'));
+}
+
+export const updateFontsStyle = () => {
+  const fontsStyle = 'app/scss/_fonts.scss';
+
+  fs.readdir('app/fonts', (err, fontsFiles) => {
+    if (fontsFiles) {
+      if (!fs.existsSync(fontsStyle)) {
+        fs.writeFile(fontsStyle, '', cb);
+
+        console.log(fontsFiles.length);
+
+        fontsFiles.forEach((fontFile) => {
+          if (fontFile.includes('ttf')) {
+          const fontFileName = fontFile.split('.')[0];
+
+          const fontName = fontFileName.split('-')[0];
+          let fontWeight = fontFileName.split('-')[1].toLowerCase();
+
+          switch (fontWeight) {
+            case 'light':
+              fontWeight = '300';
+              break;
+            case 'regular':
+              fontWeight = '400';
+              break;
+            case 'medium':
+              fontWeight = '500';
+              break;
+            case 'semibold':
+              fontWeight = '600';
+              break;
+            case 'bold':
+              fontWeight = '700';
+              break;
+            case 'extrabold':
+              fontWeight = '800';
+              break;
+            case 'black':
+              fontWeight = '900';
+              break;
+
+            default:
+              fontWeight = '400';
+          }
+
+          fs.appendFile(fontsStyle,
+            `@font-face {
+  font-family: ${fontName};
+  font-display: swap;
+  font-style: normal;
+  font-weight: ${fontWeight};
+  src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
+}\r\n`, cb);
+          }
+        })
+
+      } else {
+        console.log('_fonts.scss file already exists!');
+      }
+    } else {
+      console.log('No fonts file availible!');
+    }
+  });
+
+  return gulp.src('app/');
+  function cb() { }
 }
 
 // Build
@@ -77,9 +160,11 @@ let copyToDist = () => {
   return gulp.src([
     'app/**/*.html',
     'app/js/**/*min.js',
-    'app/css/**/*.css'
-  ], {base: 'app'})
-  .pipe(gulp.dest('dist/'))
+    'app/css/**/*.css',
+    'app/fonts/*.woff',
+    'app/fonts/*.woff2'
+  ], { base: 'app' })
+    .pipe(gulp.dest('dist/'))
 }
 
 export const build = gulp.series(clearDist, images, copyToDist);
